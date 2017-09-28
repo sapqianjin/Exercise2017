1e63ed5ad3e5b31b9e621524d58cf2fe7f2549fc
sapqianjin <sapqianjin@gmail.com>
2017-09-28 09:08:41 +0800

    Init GitHub, use Local Python Exercise2017, including Crossin Course, Automatic.

--- Automatic/SAP_MM02_SaftyStock.py
+++ Automatic/SAP_MM02_SaftyStock.py
@@ -12,15 +12,19 @@ import time, codecs
 # So we can set small pause time for some local steps,
 # but for these steps need return information from server,
 # we need set buffer time, I prefer 1 second (at least 0.5 second).
+# The buffer time must be adjusted based on the network!
 pyautogui.PAUSE = 0.1
 pyautogui.FAILSAFE = True
 bufferSecond = 1
 
 # Before start this program, the SAP GUI window must be open first,
 # The t-code MM02 should be started, and set default in view/org
-# SAP GUI use the Corbu Theme, the color will be used in program logic
+# SAP GUI use the Corbu theme, the color will be used in program logic
 # If you run it in your computer, the position/color maybe need adjust.
 # You can use pyautogui.position() get the position value.
+
+# All position are based on Win 10, task bar in left side
+# All color are based on Corbu theme
 # SAP Program SAPLMGMM Screen 0060
 posX_materialNumber = 430
 posY_materialNumber = 330
@@ -98,32 +102,14 @@ except KeyboardInterrupt:
 # Switch back to Python window, so user could know the program is finished.
 pyautogui.hotkey('alt', 'tab')
 
-# 0.5 second is accept in Client_C test, for each material, avg time 5~7s
-# test 100 material: 2017-09-27 19:44:55 ~ 2017-09-27 19:55:09
+# 0.5 second is accept , for each material, avg time 5~7s
 # So the efficiency is very low, but # 0.25 is not accept,
 # because program will place safety stock in material description field.
 # Actually, even I set 0.5 second, the program still change one
 # material description wrong in 19:49:23. (only run no more than 50 material).
-#
-# So I prefer change the PAUSE time to 1 second.
+# So I prefer change the PAUSE time to 1 second at least.
 
-# some test result:
-# Pause 0.5 second
-# test 100 material with view, org:
-# 2017-09-27 19:44:55 ~ 2017-09-27 19:55:09
-# average: each material 6.15 s
-# exist error change for material description.
-
-# Pause 1 second
-# test 20 material without view, org:
-# 2017-09-27 20:04:58 ~ 2017-09-27 20:08:21
-# average: each material 10.15 s
-# not exist error.
-# test 50 material with view, org:
-# 2017-09-27 20:16:25 ~ 2017-09-27 20:26:32
-# average: each material 12.15 s
-# not exist error.
-
+# Some test result in Client_C test:
 # Pause 0.1 second, buffer 1 Second
 # test 50 material with view, org:
 # 2017-09-27 20:46:28 ~ 2017-09-27 20:50:56
