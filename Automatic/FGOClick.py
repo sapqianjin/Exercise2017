4a3eb6ebe58ba10cf2af0fe0c6cdc0445a75e9e1
sapqianjin <sapqianjin@gmail.com>
2017-11-24 14:07:49 +0800

    Init GitHub, use Local Python Exercise2017, including Crossin Course, Automatic.

--- Automatic/FGOClick.py
+++ Automatic/FGOClick.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Dorian Wang 2017.11.21~2017.11.21
+# Dorian Wang 2017.11.21~2017.11.24
 # Auto Click FGO
 # add to GitHub
 
@@ -14,14 +14,14 @@ pyautogui.FAILSAFE = True
 width, height = pyautogui.size()
 
 
-def repeatClick1(repeatTime, position):
+def repeatClick1(position, repeatTime=1):
     # 简单的单个位置重复点击
     try:
         posX = position[0]
         posY = position[1]
         for i in range(0, repeatTime):
             # 发现模拟器窗口无法点击，也无法按键切换回去
-            # 只能使用ApowerMirror，测试OK
+            # 只能使用Apower Mirror，测试OK
             pyautogui.click(posX, posY)
             print(i, time.strftime("%H:%M:%S", time.localtime()))
             pyautogui.time.sleep(waitingSecond)
@@ -118,7 +118,8 @@ def attack(cards):
     try:
         # open attack
         pyautogui.click(2000, 1000)
-        for card in cards():
+        pyautogui.time.sleep(3)
+        for card in cards:
             # click card
             if card == 1:
                 pyautogui.click(550, 1000)
@@ -131,29 +132,29 @@ def attack(cards):
             elif card == 5:
                 pyautogui.click(2000, 1000)
             elif card == 6:
-                pyautogui.click(950, 600)
+                pyautogui.click(950, 500)
             elif card == 7:
-                pyautogui.click(1300, 600)
+                pyautogui.click(1300, 500)
             elif card == 8:
-                pyautogui.click(1600, 600)
+                pyautogui.click(1600, 500)
     except KeyboardInterrupt:
         print('User Break.')
     return ()
 
 
-def summerByFriendPoint(repeatTime=20):
+def summonByFriendPoints(repeatTime=20):
     # 友情点召唤
     try:
         for i in range(0, repeatTime):
-            # click Summer button
+            # click summon button
             pyautogui.click(1500, 1175)
-            # select summer 10 cards
+            # select summon 10 cards
             pyautogui.click(1475, 1050)
-            # confirm summer
+            # confirm summon
             pyautogui.click(1475, 1050)
             # waiting for result
             pyautogui.time.sleep(3)
-            # click to finish summer
+            # click to finish summon
             pyautogui.click(1500, 1175)
             pyautogui.time.sleep(1)
             print(i, time.strftime("%H:%M:%S", time.localtime()))
@@ -186,6 +187,8 @@ def NeroFestAutumnExpertLastRound(target=1):
     servantSkill(3, 1, target)
     # 默认按照孔明、贞德Alter、兰斯洛特依次使用宝具卡
     attack((8, 7, 6))
+    pyautogui.time.sleep(30)
+    repeatClick1((1600, 1100), 5)
     return ()
 
 
@@ -197,10 +200,12 @@ pyautogui.time.sleep(bufferSecond)
 print(time.strftime("%Y-%m-%d %H:%M:%S\n", time.localtime()))
 
 # 友情点召唤
-# summerByFriendPoint(25)
+# summonByFriendPoints(25)
 
-# 尼禄祭无限池抽奖
-# repeatClick1(10, (1000, 800))
+# 尼禄祭无限池抽奖，
+# 至少需要点击600/20*4=120次
+# 重复次数取决于网络信号，使用130-150，大约4-5分钟。
+# repeatClick1((1000, 800), 80)
 
 # 尼禄祭花瓣池第一回合
 # NeroFestAutumnExpertFirstRound()
